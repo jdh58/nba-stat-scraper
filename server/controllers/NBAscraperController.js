e6f64d5eba26821a34a76135fe9806ed26e563d8
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-06-18 06:14:42 -0700

    Grab player accolades, nicknames, draft pos and yr

--- server/controllers/NBAscraperController.js
+++ server/controllers/NBAscraperController.js
@@ -1,20 +1,75 @@
 import cheerio from 'cheerio';
 
 const getPlayer = async (req, res) => {
-  // Grab player's name from URL and format it for BBREF
-  let playerName = req.params.playerName;
-  let [first, last] = playerName.split('_');
+  try {
+    // Grab player's name from URL and format it for BBREF
+    let playerName = req.params.playerName;
+    let [first, last] = playerName.split('_');
 
-  const response = await fetch(
-    `https://www.basketball-reference.com/search/?&search=${first}+${last}`
-  );
-  const responseHTML = await response.text();
+    const searchResponse = await fetch(
+      `https://www.basketball-reference.com/search/?&search=${first}+${last}`
+    );
+    const searchResponseHTML = await searchResponse.text();
 
-  const $ = cheerio.load(responseHTML);
-  const firstResult = $('.search-item-name');
+    let $ = cheerio.load(searchResponseHTML);
+    const firstResultHREF = $('.search-item-name')
+      .children(':nth-child(1)')
+      .children(':nth-child(1)')
+      .attr('href');
 
-  console.log(firstResult.attr('class'));
-  res.send('s');
+    const playerResponse = await fetch(
+      `https://www.basketball-reference.com/${firstResultHREF}`
+    );
+    const playerResponseHTML = await playerResponse.text();
+
+    $ = cheerio.load(playerResponseHTML);
+
+    // First, grab the player's accolades
+    const accolades = [];
+    const accoladeList = $('#bling');
+
+    accoladeList.children().each((index, accolade) => {
+      const accoladeElement = $(accolade);
+      accolades.push(accoladeElement.text());
+    });
+
+    // FIND THESE IN A MORE PROGRAMMATIC WAY THE NTH CHILD SHIT MOVES AROUND ARGH
+    // Now fetch nicknames
+    const nicknames = [];
+    const nicknameList = $('#meta')
+      .children(':nth-child(2)')
+      .children(':nth-child(3)')
+      .text();
+
+    const nicknameArr = nicknameList.replace(/[()\n]/g, '').split(',');
+
+    for (let i = 0; i < nicknameArr.length; i++) {
+      nicknames.push(nicknameArr[i].trim());
+    }
+
+    // Now get place of birth
+    const birthplaceElement = $('#meta')
+      .children(':nth-child(2)')
+      .children(':nth-child(6)')
+      .children(':nth-child(3)')
+      .text();
+    const birthplace = birthplaceElement.trim().slice(3);
+
+    // Draft pick and first year
+    const draftRegex = /\d+ NBA Draft/;
+    const draftElement = $('a:contains("NBA Draft")').text();
+    // const pickRegex = /\d+ NBA Draft/;
+    // const draftElement = $('p:contains("NBA Draft")').text();
+
+    // const pickNum = draftElement.match(/\d+/g)[2];
+    // const draftYear = draftElement.match(/\d+/g)[3];
+
+    console.log(draftElement);
+
+    res.send(birthplace);
+  } catch (err) {
+    console.error("Failed to fetch BBREF player's page", err);
+  }
 };
 
 export { getPlayer };
