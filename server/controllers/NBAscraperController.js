0691ab19f5e9db455517c4c8f76cd6de70b5d05a
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-06-18 23:17:18 -0700

    Remove old commented out code s

--- server/controllers/NBAscraperController.js
+++ server/controllers/NBAscraperController.js
@@ -173,66 +173,6 @@ const getPlayer = async (req, res) => {
     const headshotURL = `https://cdn.nba.com/headshots/nba/latest/1040x760/${playerID}.png`;
 
     res.send(headshotURL);
-
-    /* nicknameList will be dependent on if the player's page has
-    a) First row as a pronunciation
-    or b) No nicknames listed */
-    // let nicknameList;
-
-    // // If the first row is pronunciation
-    // if (
-    //   /Pronunciation/.test(playerInfoContainer.find('p:nth-child(2)').text())
-    // ) {
-    //   // Then nicknames will be the 4th child. If they aren't then there are no nicknames.
-    //   if (
-    //     /^\(.*\)$/.test(
-    //       playerInfoContainer.find('p:nth-child(4)').text().trim()
-    //     )
-    //   ) {
-    //     nicknameList = playerInfoContainer.find('p:nth-child(4)').text();
-    //   } else {
-    //     nicknameList = null;
-    //   }
-    // } else {
-    //   // If there is no pronunciation row, then nicknames will be the 3rd child.
-    //   // If they aren't, then there are no nicknames.
-    //   if (
-    //     /^\(.*\)$/.test(
-    //       playerInfoContainer.find('p:nth-child(3)').text().trim()
-    //     )
-    //   ) {
-    //     nicknameList = playerInfoContainer.find('p:nth-child(3)').text();
-    //   } else {
-    //     nicknameList = null;
-    //   }
-    // }
-
-    // if (nicknameList) {
-    //   const nicknameArr = nicknameList.replace(/[()\n]/g, '').split(',');
-
-    //   for (let i = 0; i < nicknameArr.length; i++) {
-    //     nicknames.push(nicknameArr[i].trim());
-    //   }
-    // }
-
-    // // Now get place of birth
-    // const birthplaceElement = $('#meta')
-    //   .children(':nth-child(2)')
-    //   .children(':nth-child(6)')
-    //   .children(':nth-child(3)')
-    //   .text();
-    // const birthplace = birthplaceElement.trim().slice(3);
-
-    // // Draft pick and first year
-    // const draftRegex = /\d+ NBA Draft/;
-    // const draftElement = $('a:contains("NBA Draft")').text();
-    // const pickRegex = /\d+ NBA Draft/;
-    // const draftElement = $('p:contains("NBA Draft")').text();
-
-    // const pickNum = draftElement.match(/\d+/g)[2];
-    // const draftYear = draftElement.match(/\d+/g)[3];
-
-    // console.log(draftElement);
   } catch (err) {
     console.error("Failed to fetch BBREF player's page", err);
   }
