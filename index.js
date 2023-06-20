c62ec14d210dd40f4a4d62fb9605602ca0f228a9
Jonathan Hawes <jonathan@jonathanhawes.dev>
2023-06-20 14:54:36 -0700

    Add many more stats to getTeamSeason()

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const getPlayer = require('./modules/getPlayer');
 const getTeam = require('./modules/getTeam');
 const getPlayerSeason = require('./modules/getPlayerSeason');
 const getPlayerCareer = require('./modules/getPlayerCareer');
+const getTeamSeason = require('./modules/getTeamSeason');
 
 (async () => {
-  const banana = await getPlayer('kevin durant');
-  console.log(JSON.parse(banana));
+  const banana = await getTeamSeason('hawks', 1998);
+  // console.log(JSON.parse(banana));
 })();
--- modules/getTeamSeason.js
+++ modules/getTeamSeason.js
@@ -31,4 +31,92 @@ async function getTeamSeason(teamName, year) {
     roster: ["boob ryan"]
   }
   */
+
+  const searchQuery = `${year} ${teamName}`;
+
+  const searchResponse = await fetch(
+    `https://www.basketball-reference.com/search/?&search=${searchQuery}`
+  );
+  // On BBREF, year + team auto redirects to that team's page
+  const firstResultHTML = await searchResponse.text();
+
+  $ = cheerio.load(firstResultHTML);
+
+  console.log(searchQuery);
+  // First, grab the team's name.
+  const name = $('#meta h1').text().trim();
+
+  // Check if we got to a team's page or not.
+  if (name.length <= 0) {
+    throw new Error(
+      'Search query provided no results. Make sure your search query works at https://basketball-reference.com/'
+    );
+  }
+
+  let wins = -1;
+  let losses = -1;
+  let seed = -1;
+  let coach = '';
+  let executive = '';
+  let ppg = -1;
+  let oppg = -1;
+  let srs = -1;
+  let pace = -1;
+  let expW = -1;
+  let expL = -1;
+  let preseasonOdds = -1;
+  let overUnder = -1;
+
+  // First, grab all the basic information from the team's bio
+  const bio = $('#meta > div:nth-child(2) > p');
+
+  bio.each((index, element) => {
+    const elementData = $(element);
+    const elementText = $(element).text();
+
+    if (/Record:/.test(elementText)) {
+      [wins, losses, seed] = elementText.match(/\d+/g);
+      [wins, losses, seed] = [parseInt(wins), parseInt(losses), parseInt(seed)];
+    } else if (/Coach:/.test(elementText)) {
+      coach = elementData.find('a').text();
+    } else if (/Executive:/.test(elementText)) {
+      executive = elementData.find('a').text();
+    } else if (/PTS\/G:/.test(elementText)) {
+      console.log(elementText);
+      [ppg, oppg] = elementText.match(/\d+\.\d/g);
+      [ppg, oppg] = [parseFloat(ppg), parseFloat(oppg)];
+    } else if (/SRS:/.test(elementText)) {
+      [srs, pace] = elementText.match(/\d+\.\d+/g);
+      [srs, pace] = [parseFloat(srs), parseFloat(pace)];
+    } else if (/Off Rtg:/.test(elementText)) {
+      [oRtg, dRtg] = elementText.match(/\d+\.\d/g);
+      [oRtg, dRtg] = [parseFloat(oRtg), parseFloat(dRtg)];
+      netRtg = parseFloat(elementText.match(/[\+-]\d+\.\d/g)[0]);
+    } else if (/Expected W-L:/.test(elementText)) {
+      expW = parseInt(elementText.match(/\d+/)[0]);
+      expL = parseInt(elementText.match(/\d+/g)[1]);
+    } else if (/Preseason Odds:/.test(elementText)) {
+      preseasonOdds = parseInt(elementText.match(/[\+-]\d+/));
+      overUnder = parseFloat(elementText.match(/\d+\.\d/g)[0]);
+    }
+  });
+
+  console.log(wins);
+  console.log(losses);
+  console.log(seed);
+  console.log(coach);
+  console.log(executive);
+  console.log(ppg);
+  console.log(oppg);
+  console.log(srs);
+  console.log(pace);
+  console.log(oRtg);
+  console.log(dRtg);
+  console.log(netRtg);
+  console.log(expW);
+  console.log(expL);
+  console.log(preseasonOdds);
+  console.log(overUnder);
 }
+
+module.exports = getTeamSeason;
